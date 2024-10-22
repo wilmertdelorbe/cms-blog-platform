7f5e6e1336aca691fbf1b99196d4bcf2fde0da2a
wilmertdelorbe <167813507+wilmertdelorbe@users.noreply.github.com>
2024-10-22 02:00:29 -0400

    restored env and db js

--- config/db.js
+++ config/db.js
@@ -21,8 +21,7 @@ if (process.env.DATABASE_URL) {
     process.env.DB_PASSWORD,
     {
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: 'postgres',
+      dialect: process.env.DB_DIALECT,
       logging: false
     }
   );
