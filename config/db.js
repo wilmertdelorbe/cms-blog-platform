77cd98cfc4fe43eef97ecfa5be59c0255afe65d9
wilmertdelorbe <167813507+wilmertdelorbe@users.noreply.github.com>
2024-10-22 00:30:11 -0400

    updated to render

--- config/db.js
+++ config/db.js
@@ -1,28 +1,26 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-let dbConnection;
+const dbConfig = {
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'postgres',
+  logging: false,
+};
 
-if (process.env.DATABASE_URL) {
-  dbConnection = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
+if (process.env.NODE_ENV === 'production') {
+  dbConfig.dialectOptions = {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
-  });
-} else {
-  dbConnection = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST,
-      dialect: 'postgres'
-    }
-  );
+  };
 }
 
-module.exports = dbConnection;
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  dbConfig
+);
+
+module.exports = sequelize;
